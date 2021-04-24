b2f0525afcb414fd5b6a5173839a1d09751a990c
Cao, Karl <yixiang9@illinois.edu>
2021-04-24 17:31:49 +0800

    Update airport.cpp

--- airport.cpp
+++ airport.cpp
@@ -7,3 +7,16 @@ void airport::print_airport() {//print out all the variables of airports
     std::cout<<"Country: "<<country<<std::endl;
     std::cout<<"Location: "<<longitude<<", "<<latitude<<endl;
 }
+    
+Airport::Airport() {
+
+}
+
+Airport::Airport(std::string n, std::string c, double lat, double lon, std::string id) {
+    cord = Coordinate(lat, lon);
+    name = n;
+    city = c;
+    ID = id;
+    latitude = lat;
+    longitude = lon;
+}
