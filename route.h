216509f89058d3c71b7c6e7f448a92b5baf118d2
yunfan <yunfany3@illinois.edu>
2021-04-24 01:42:21 +0000

    air line ID added

--- route.h
+++ route.h
@@ -4,7 +4,9 @@
 class route {
 public:
     airport depature;
+    std::string depature_id;
     airport destination;
+    std::string destination_id;
     bool is_air;
     double distance;
     //double weight;
