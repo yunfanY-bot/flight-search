bf507ba064a33374a66d6d0fed2dc85898394f19
jiayuz6 <jiayuz6@illinois.edu>
2021-05-05 14:20:53 +0800

    jiayu

--- graph.cpp
+++ graph.cpp
@@ -1,19 +1,22 @@
 #include "graph.h"
 
 
-Graph::Graph() {
-    weighted = true;
-    directed = false;
-    mutable unordered_map<Airport, unordered_map<Airport, route>>() a;
-    adjacency = a;
 
+const Airport Graph::InvalidVertex = Airport();
 
+Graph::Graph(bool weighted) : weighted(weighted),directed(true)
+{
 }
+
+Graph::Graph(bool weighted, bool directed) : weighted(weighted),directed(directed)
+{
+}
+
+
 
 vector<Airport> Graph::getAdjacent(Airport source) const 
 {
     auto lookup = adjacency.find(source);
-
     if(lookup == adjacency.end()){
         return vector<Airport>();
     } else {
@@ -30,15 +33,69 @@ vector<Airport> Graph::getAdjacent(Airport source) const
 void Graph::insertVertex(Airport a) 
 {
     removeVertex(a);
-    adjacencyt[a] = unordered_map<Airport, route>();
+    adjacency[a] = unordered_map<Airport, route>();
+}
+
+Airport Graph::removeVertex(Airport v)
+{
+
+    if (adjacency.find(v) != adjacency.end())
+    {
+        if(!directed){
+            for (auto it = adjacency[v].begin(); it != adjacency[v].end(); it++)
+            {
+                Airport u = it->first;
+                adjacency[u].erase(v); 
+            }
+            adjacency.erase(v);
+            return v;
+        }
+        
+        adjacency.erase(v);
+        for(auto it2 = adjacency.begin(); it2 != adjacency.end(); it2++)
+        {
+            Airport u = it2->first;
+            if (it2->second.find(v)!=it2->second.end())
+            {
+                it2->second.erase(v);
+            }
+        }
+        return v;
+    }
+    return InvalidVertex;//return empty airport 
+
 }
 
 
-bool Graph::assertVertexExists(Airport v) const
+
+bool Graph::ifVertexExists(Airport v) const
 {
-    if (adjacency_list.find(v) == adjacency_list.end())
+    if (adjacency.find(v) == adjacency.end())
     {
         return false;
+    }
+    return true;
+}
+
+
+bool Graph::ifEdgeExists(Airport source, Airport destination) const
+{
+    if(ifVertexExists(source) == false)
+        return false;
+    if(adjacency[source].find(destination)== adjacency[source].end())
+    {
+        
+        return false;
+    }
+    if(!directed)
+    {
+        if (ifVertexExists(destination) == false)
+            return false;
+        if(adjacency[destination].find(source)== adjacency[destination].end())
+        {
+            
+            return false;
+        }
     }
     return true;
 }
--- graph.h
+++ graph.h
@@ -4,14 +4,17 @@
 #include <unordered_map>
 #include <utility>
 #include <algorithm>
-
+#include <string>
+#include <cstdlib>
+#include <climits>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <set>
 #include <sstream>
 #include <vector>
 #include "airport.h"
-
+#include <map>
 #include "route.h"
 
 using namespace std;
@@ -20,12 +23,14 @@ using namespace std;
 class Graph {
     public:
     //draw graph use helper functions in Airport and route classes
-    Graph();//definitely a weighted graph
-    Airport getAdjacent(Airport source) const;//get all adj vertices
+    Graph(bool weighted);
+    Graph(bool weighted, bool directed);
+    
+    vector<Airport> getAdjacent(Airport source) const;//get all adj vertices
     Airport getVertices() const; //gets all vertices, debug used
     route getRoutes(Airport source, Airport destination) const;//find a route between 2 airports
-    void insertVertex();//insert an airport
-    void removeVertex();//remove an airport
+    void insertVertex(Airport a);//insert an airport
+    Airport removeVertex(Airport a);//remove an airport
     bool insertRoutes(Airport source, Airport destination);//build route between 2 airports (seems fixed when parsing the txt?)
     route removeRoutes(Airport source, Airport destination);
     route setRoutedistance(Airport source, Airport destination, int distance);//use weight to set distance
@@ -38,8 +43,9 @@ class Graph {
     bool weighted; //if weighted
     bool directed;
     mutable unordered_map<Airport, unordered_map<Airport, route>> adjacency;
-    bool assertVertexExists(Airport v) const; //check if exists helper func
-    bool assertEdgeExists(Airport source, Airport destination) const; // check if exists helper func
+    bool ifVertexExists(Airport v) const; //check if exists helper func
+    bool ifEdgeExists(Airport source, Airport destination) const; // check if exists helper func 如果没有才能insert
+    const static Airport InvalidVertex;
 
 
 
